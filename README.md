1943d68d186c632862b00e0c0b13de75435237d7
Tiago Gouvêa <tiago@tiagogouvea.com.br>
2020-12-02 06:26:00 -0300

    Projeto react adicionado

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Code Instagram Clone
 
+Projeto de estudo do treinamento "React para humanos" sendo oferecido pela [App Masters](https://appmasters.io/) para a [Code Júnior](https://codejr.com.br/).
+
 O backend do projeto está em [josueodh/backend-treinamento-node](https://github.com/josueodh/backend-treinamento-node).
+
+# Como executar
+
+Para executar o projeto em sua máquina local:
+
+```shell script
+git clone https://github.com/app-masters-academy/code-instagram-clone.git
+cd code-instagram-clone
+yarn install
+yarn start
+```
+
+Neste momento você poderá acessar [http://localhost:3000/](http://localhost:3000/) e ver sua aplicação em fucionamento.
---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Code Instagram Clone
+        </p>
+        <a
+          className="App-link"
+          href="https://appmasters.io/"
+          target="_blank"
+        >
+          App Masters
+        </a>
+        <a
+            className="App-link"
+            href="https://codejr.com.br/"
+            target="_blank"
+        >
+          Code Júnior
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
